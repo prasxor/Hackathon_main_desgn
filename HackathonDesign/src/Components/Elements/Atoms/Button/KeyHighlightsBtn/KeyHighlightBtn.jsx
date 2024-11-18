e8c3a9c6c8cd526841d9396e0a03dbd1dd4c4708
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2024-11-18 16:05:47 +0530

    updated without any error

--- HackathonDesign/src/Components/Elements/Atoms/Button/KeyHighlightsBtn/KeyHighlightBtn.jsx
+++ HackathonDesign/src/Components/Elements/Atoms/Button/KeyHighlightsBtn/KeyHighlightBtn.jsx
@@ -1,11 +1,59 @@
-// import React from "react";
+import React, { useState } from "react";
+import "./KeyHighlightBtn.css";
+
+const KeyHighlightBtn = ({ image, text, onClick }) => {
+  const [rotation, setRotation] = useState(0);
+
+  // Updated handleClick to handle both image rotation and onClick
+  const handleClick = () => {
+    setRotation((prevRotation) => prevRotation + 720); // Rotate by 720 degrees
+    if (onClick) onClick(); // Trigger the onClick function passed from the parent
+  };
+
+  return (
+    <div className="KeyHighlightBtnMain">
+      {/* Apply handleClick to the entire button */}
+      <button onClick={handleClick}>
+        <img
+          src={image}
+          alt=""
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            transition: "transform 1s ease-in-out",
+          }}
+        />
+        <span>{text}</span>
+      </button>
+    </div>
+  );
+};
+
+export default KeyHighlightBtn;
+
+
+
+
+// import React, { useState } from "react";
 // import "./KeyHighlightBtn.css";
 
-// const KeyHighlightBtn = ({ image, text }) => {
+// const KeyHighlightBtn = ({ image, text, onClick }) => {
+//   const [rotation, setRotation] = useState(0); // Track the rotation angle
+
+//   const handleClick = () => {
+//     setRotation((prevRotation) => prevRotation + 720); // Increment by 720 degrees each time
+//   };
+
 //   return (
 //     <div className="KeyHighlightBtnMain">
-//       <button>
-//         <img src={image} alt="" />
+//       <button onClick={handleClick}>
+//         <img
+//           src={image}
+//           alt=""
+//           style={{
+//             transform: `rotate(${rotation}deg)`, // Correctly apply rotation
+//             transition: "transform 1s ease-in-out",
+//           }}
+//         />
 //         <a href="#">{text}</a>
 //       </button>
 //     </div>
@@ -13,34 +61,4 @@
 // };
 
 // export default KeyHighlightBtn;
-
-
-import React, { useState } from "react";
-import "./KeyHighlightBtn.css";
-
-const KeyHighlightBtn = ({ image, text }) => {
-  const [rotation, setRotation] = useState(0); // Track the rotation angle
-
-  const handleClick = () => {
-    setRotation((prevRotation) => prevRotation + 720); // Increment by 720 degrees each time
-  };
-
-  return (
-    <div className="KeyHighlightBtnMain">
-      <button onClick={handleClick}>
-        <img
-          src={image}
-          alt=""
-          style={{
-            transform: `rotate(${rotation}deg)`, // Correctly apply rotation
-            transition: "transform 1s ease-in-out",
-          }}
-        />
-        <a href="#">{text}</a>
-      </button>
-    </div>
-  );
-};
-
-export default KeyHighlightBtn;
 
--- HackathonDesign/src/Components/Elements/ThirdHome/ThirdHome.jsx
+++ HackathonDesign/src/Components/Elements/ThirdHome/ThirdHome.jsx
@@ -3,34 +3,36 @@ import "./ThirdHome.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import FakeNewsCards from "../Atoms/Cards/FakeNewsCards/FakeNewsCards";
-import FakeData from "../../Data/TrendingNewsData.json";
 import KeyHighlights from "../Atoms/KeyHighlights/KeyHighlights";
 import KeyHighlightBtn from "../Atoms/Button/KeyHighlightsBtn/KeyHighlightBtn";
 import RefreshBtn from "../../../assets/RefreshBtn.svg";
-import Img1 from "../../../assets/FakeNewsImg1.jpg";
-import Img2 from "../../../assets/FakeNewsImg2.avif";
-import Img3 from "../../../assets/FakeNewsImg3.jpg";
-import Img4 from "../../../assets/FakeNewsImg4.webp";
-import Img5 from "../../../assets/FakeNewsImg5.webp";
-import Img6 from "../../../assets/FakeNewsImg6.jpg";
 import leftArrow from "../../../assets/left_Arrow_card.png";
 import rightArrow from "../../../assets/Right_Arrow_card.png";
+import FakeNewsData from "../../Data/FakeNewsData.json";
 
 const ThirdHome = () => {
   const swiperRef = useRef(null);
 
-  // Array of images to pass dynamically
-  const images = [Img1, Img2, Img3, Img4, Img5, Img6];
+  const [startIndex, setStartIndex] = useState(0);
+
+  const displayedNews = FakeNewsData.FakeNews.slice(startIndex, startIndex + 10);
+
+
+  const handleRefreshClick = () => {
+    setStartIndex((prevIndex) => {
+      const nextIndex = prevIndex + 10;
+      return nextIndex >= FakeNewsData.FakeNews.length ? 0 : nextIndex;
+    });
+    if (swiperRef.current) swiperRef.current.swiper.slideTo(0);
+  };
 
   const handlePrevClick = () => {
-    swiperRef.current.swiper.slidePrev();
+    if (swiperRef.current) swiperRef.current.swiper.slidePrev();
   };
 
   const handleNextClick = () => {
-    swiperRef.current.swiper.slideNext();
+    if (swiperRef.current) swiperRef.current.swiper.slideNext();
   };
-
-
 
   return (
     <div className="ThirdHomeContainer">
@@ -39,9 +41,10 @@ const ThirdHome = () => {
       </div>
       <div className="ThirdHomeContainerTitle">
         <KeyHighlightBtn
-        image={RefreshBtn}
-        text="Refresh"
-      />
+          image={RefreshBtn}
+          text="Refresh"
+          onClick={handleRefreshClick}
+        />
         <p>Fake News</p>
         <KeyHighlightBtn text={"View All"} />
       </div>
@@ -57,22 +60,17 @@ const ThirdHome = () => {
         {/* Swiper Container */}
         <div id="FakeNewsCardsMainContainerSub">
           <Swiper
-            spaceBetween={20} // Space between each card
-            slidesPerView={3} // Show 3 cards at a time
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
+            spaceBetween={20}
+            slidesPerView={3}
             ref={swiperRef}
           >
-            {FakeData.News.FakeNews.map((news, index) => (
+            {displayedNews.map((news, index) => (
               <SwiperSlide key={index}>
                 <FakeNewsCards
-                  Title={news.TITLE}
-                  Image={images[index % images.length]}
-                  Para={
-                    "We send you timely email notifications about trending fake news stories relevant to your interests, helping you stay informed and vigilant"
-                  }
+                  Title={news.title}
+                  Image={news.image}
+                  Para={news.SUMMARY}
                 />
-                {/* <FakeNewsCards Image={images[0]} Title={"Lorem ipsum dolor sit amet"} Para={"We send you timely email notifications about trending fake news stories relevant to your interests, helping you stay informed and vigilant."}/> */}
               </SwiperSlide>
             ))}
           </Swiper>
@@ -91,3 +89,96 @@ const ThirdHome = () => {
 };
 
 export default ThirdHome;
+
+
+
+// import React, { useRef, useState } from "react";
+// import "./ThirdHome.css";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import FakeNewsCards from "../Atoms/Cards/FakeNewsCards/FakeNewsCards";
+// import FakeNewsData from "../../Data/TrendingNewsData.json";
+// import KeyHighlights from "../Atoms/KeyHighlights/KeyHighlights";
+// import KeyHighlightBtn from "../Atoms/Button/KeyHighlightsBtn/KeyHighlightBtn";
+// import RefreshBtn from "../../../assets/RefreshBtn.svg";
+// import Img1 from "../../../assets/FakeNewsImg1.jpg";
+// import Img2 from "../../../assets/FakeNewsImg2.avif";
+// import Img3 from "../../../assets/FakeNewsImg3.jpg";
+// import Img4 from "../../../assets/FakeNewsImg4.webp";
+// import Img5 from "../../../assets/FakeNewsImg5.webp";
+// import Img6 from "../../../assets/FakeNewsImg6.jpg";
+// import leftArrow from "../../../assets/left_Arrow_card.png";
+// import rightArrow from "../../../assets/Right_Arrow_card.png";
+
+// const ThirdHome = () => {
+//   const swiperRef = useRef(null);
+
+//   // Array of images to pass dynamically
+//   const images = [Img1, Img2, Img3, Img4, Img5, Img6];
+
+//   const handlePrevClick = () => {
+//     swiperRef.current.swiper.slidePrev();
+//   };
+
+//   const handleNextClick = () => {
+//     swiperRef.current.swiper.slideNext();
+//   };
+
+//   return (
+//     <div className="ThirdHomeContainer">
+//       <div className="ThirdHomeContainerKeyHightlights">
+//         <KeyHighlights text={"Trending"} />
+//       </div>
+//       <div className="ThirdHomeContainerTitle">
+//         <KeyHighlightBtn
+//         image={RefreshBtn}
+//         text="Refresh"
+//       />
+//         <p>Fake News</p>
+//         <KeyHighlightBtn text={"View All"} />
+//       </div>
+//       <div className="FakeNewsCardsMainContainer">
+//         {/* Left Arrow */}
+//         <div
+//           className="FakeNewsCardsMainContainerLeft"
+//           onClick={handlePrevClick}
+//         >
+//           <img src={leftArrow} alt="Left Arrow" />
+//         </div>
+
+//         {/* Swiper Container */}
+//         <div id="FakeNewsCardsMainContainerSub">
+//           <Swiper
+//             spaceBetween={20}
+//             slidesPerView={3}
+//             onSlideChange={() => console.log("slide change")}
+//             onSwiper={(swiper) => console.log(swiper)}
+//             ref={swiperRef}
+//           >
+//             {FakeNewsData.News.FakeNews.map((news, index) => (
+//               <SwiperSlide key={index}>
+//                 <FakeNewsCards
+//                   Title={news.TITLE}
+//                   Image={images[index % images.length]}
+//                   Para={
+//                     "We send you timely email notifications about trending fake news stories relevant to your interests, helping you stay informed and vigilant"
+//                   }
+//                 />
+//               </SwiperSlide>
+//             ))}
+//           </Swiper>
+//         </div>
+
+//         {/* Right Arrow */}
+//         <div
+//           className="FakeNewsCardsMainContainerRight"
+//           onClick={handleNextClick}
+//         >
+//           <img src={rightArrow} alt="Right Arrow" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ThirdHome;
